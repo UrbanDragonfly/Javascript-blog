b3f17e9b62566302041f27ea3280dd672d37c193
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-11 15:59:05 +0200

    add code to make clickedElement active

--- js/script.js
+++ js/script.js
@@ -7,9 +7,10 @@
 */
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event:', event);
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -19,8 +20,13 @@
     }
   
     /* add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+
+    clickedElement.classList.add('active');
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
