d7933480c898a6c49e6589b33070803721dc1227
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-11 18:28:46 +0200

    add code to select and display articles

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 */
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event:', event);
@@ -19,7 +20,7 @@
         activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
     console.log('clickedElement (with plus): ' + clickedElement);
@@ -31,15 +32,25 @@
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
     for(let activeArticle of activeArticles){
-        console.log('remove active articles')
+        console.log('remove active articles');
         activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log (targetArticle);
   
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
+
   }
   
   const links = document.querySelectorAll('.titles a');
