74200099cc558075e7a415a516b69586ef307057
Michal Kozlowski <michal.a.kozlowski@live.com>
2021-04-11 15:36:18 +0200

    add functions to remove active class from elements

--- js/script.js
+++ js/script.js
@@ -10,10 +10,24 @@
     console.log('Link was clicked!');
     console.log('event:', event);
     /* remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        console.log('removing active links')
+        activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+
+    for(let activeArticle of activeArticles){
+        console.log('remove active articles')
+        activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
